2371587e5f730f52a6fe79de10becaabb2658d26
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-10-11 15:03:01 +0500

    Add project files.

---
+++ HttpClient/Data.cs
@@ -0,0 +1,12 @@
+﻿
+public class Data
+{
+    public string title { get; set; }
+    public string code { get; set; }
+    public string cb_price { get; set; }
+    public double? nbu_buy_price { get; set; }
+    public double? nbu_cell_price { get; set; }
+    public string date { get; set; }
+
+}
+
---
+++ HttpClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+double money;
+string from, to;
+
+Console.Write("Valyutani kiriting: ");
+from = Console.ReadLine();
+
+Console.Write("Qaysi valyutaga: ");
+to = Console.ReadLine();
+
+Console.Write("Summani kiriting: ");
+money = double.Parse(Console.ReadLine());
+
+using (HttpClient client = new HttpClient())
+{
+    string BASE_URL = "https://nbu.uz/uz/exchange-rates/json/";
+
+    HttpResponseMessage responce = await client.GetAsync(BASE_URL);
+
+    string respon = await responce.Content.ReadAsStringAsync();
+
+    //Console.WriteLine(respon);
+
+    var data = JsonConvert.DeserializeObject<List<Data>>(respon);
+    string res = data.FirstOrDefault(x => x.code) == from;
+    //if ()
+    //{
+    //    Console.WriteLine("SA");
+    //}
+    //else { Console.WriteLine("MA"); }
+
+    //foreach(var item in data)
+    //{
+    //    Console.WriteLine(item.cb_price);
+    //}
+}
+
